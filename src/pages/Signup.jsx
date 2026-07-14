3ae9f941db32ff8a3321a83f8749dd3360ef57d6
Rams-888 <23ucs143@kamarajengg.edu.in>
2026-07-14 22:28:57 +0530

    Updated API URL for Render backend

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -46,7 +46,7 @@ function Signup() {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/users/signup", {
+        const response = await fetch("https://dancefloor-backend.onrender.com/api/users/signup", {
 
             method: "POST",
 
